757676291e61ce7a9eff3d1d50fb87ddfeed230a
RomaaaK <zimpsonzya@gmail.com>
2021-01-15 20:14:37 +0300

    first commit

---
+++ readme.md
@@ -0,0 +1,5 @@
+# My config webpack
+
+npm run build
+npm run dev
+npm run start
---
+++ webpack.config.js
@@ -0,0 +1,128 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const OptimazeCssAssetWebpackPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
+
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
+
+const filename = ext => isDev ? `[name].${ext}` : `[name].[contenthash].${ext}`;
+
+const optimization = () => {
+    const config = {
+        splitChunks: {
+            chunks: 'all'
+        }
+    };
+
+    if(!isDev) {
+        config.minimizer = [
+            new OptimazeCssAssetWebpackPlugin(),
+            new TerserWebpackPlugin()
+        ]
+    }
+    return config;
+}
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    mode: 'development',
+    entry: './js/main.js',
+    output: {
+        filename: `./js/${filename('js')}`,
+        path: path.resolve(__dirname, 'app'),
+        publicPath: ''
+    },
+    devServer: {
+        historyApiFallback: true,
+        contentBase: path.resolve(__dirname, 'app'),
+        open: true,
+        compress: true,
+        hot: true,
+        port: 3000,
+    },
+    optimization: optimization(),
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: path.resolve(__dirname, 'src/index.html'),
+            filename: 'index.html',
+            minify: {
+                collapseWhitespace: !isDev
+            }
+        }),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: `./css/${filename('css')}`
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, 'src/assets'),
+                    to: path.resolve(__dirname, 'app/assets')
+                }
+            ]
+        })
+    ],
+    devtool: !isDev ? false : 'source-map',
+    module: {
+        rules: [
+            {
+                test: /\.html$/i,
+                use: ['html-loader']
+            },
+            {
+                test: /\.css$/i,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            hmr: isDev
+                        },
+                    },
+                    'css-loader'
+                ],
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            publicPath: (resourcePath, context) => {
+                                return path.relative(path.dirname(resourcePath), context) + '/';
+                            }
+                        }
+                    },
+                    'css-loader',
+                    'sass-loader'
+                ],
+            },
+            {
+                    test: /\.js$/,
+                    exclude: '/node_modules',
+                    use: ['babel-loader']
+            },
+            {
+                test: /\.(?:|gif|png|jpg|jpeg|svg)$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: `./img/${filename('[ext]')}`
+                    }
+                }]
+            },
+            {
+                test: /\.(?:|woff2)$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: `./fonts/${filename('[ext]')}`
+                    }
+                }]
+            }
+        ]
+    }
+};
